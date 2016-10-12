d3c73f73620aed3bb03c002fad76c9b34effa4af
Pranotikokas <pranotib9@gmail.com>
2016-10-12 14:24:37 +0200

    new version of web file

--- app.js
+++ app.js
@@ -6,6 +6,20 @@ angular.module('NameCalculator', [])
 .controller('NameCalculatorController', function ($scope) {
   $scope.name = "";
   $scope.totalvalue = 0;
+
+  $scope.displayNumeric = function (){
+    var totalnamevalue = calculateNumericForString($scope.name);
+    $scope.totalvalue =  totalnamevalue;
+  };
+
+  function calculateNumericForString(string){
+    var totalstringvalue = 0;
+    for(var i =0; i< string.length; i++){
+      totalstringvalue += string.charCodeAt(i);
+    }
+    return totalstringvalue;
+  }
+
 });
 
 })();
